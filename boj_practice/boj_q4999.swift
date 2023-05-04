2eca936a124ed45209b41b29e2629f9e56fce20d
jujinhyeong <mhn03148@naver.com>
2023-05-05 00:48:41 +0900

    .

---
+++ boj_practice/boj_q4999.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  boj_practice
+//
+//  Created by 주진형 on 2023/05/05.
+//
+
+import Foundation
+let jae = readLine()!
+let doc = readLine()!
+
+if (jae.count < doc.count){
+    print("no")
+}
+else{
+    print("go")
+}
---
+++ boj_practice/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  boj_practice
+//
+//  Created by 주진형 on 2023/05/05.
+//
+
+import Foundation
