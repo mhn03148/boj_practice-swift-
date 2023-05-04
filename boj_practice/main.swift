612fe697b6b0709a7a0e7ca219f0936068064d57
jujinhyeong <mhn03148@naver.com>
2023-05-04 23:22:38 +0900

    test

--- boj_practice/main.swift
+++ boj_practice/main.swift
@@ -7,5 +7,14 @@
 
 import Foundation
 
-print("Hello, World!")
+let t = Int(readLine()!)!
+var ansArr = [[Int]]()
+for _ in 0..<t{
+    let a = readLine()!.split(separator: " ").map{Int(String($0))!}
+    ansArr.append(a)
+}
+ansArr.sorted { lhs, rhs ->Bool in return lhs[0] < rhs[0] || (lhs[0] == rhs[0] && lhs[1] < rhs[1])
+}.forEach{ ansArry in
+    print("\(ansArry[0]) \(ansArry[1])")
+}
 
