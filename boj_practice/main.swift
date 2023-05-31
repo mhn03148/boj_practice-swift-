0958459ab93bdc8e8764909a03b89be4def058ac
jujinhyeong <mhn03148@naver.com>
2023-05-31 22:29:42 +0900

    .

---
+++ boj_practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  boj_practice
+//
+//  Created by 주진형 on 2023/05/31.
+//
+
+import Foundation
+var numArray = readLine()!.split(separator: " ")
+
+numArray.sort()
+print(numArray[1])
